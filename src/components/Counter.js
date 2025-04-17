bceeb13b5c10a3bfca671d378ad56b549af104ed
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-04-17 10:17:20 +0300

    multiple state management

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,28 +3,36 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
 
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector(state => state.counter);
+  const isVisible = useSelector(state => state.toggleVisible);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
     dispatch({type: 'increment'});
   }
 
+  const increaseBy5Handler = () => {
+    dispatch({ type: 'increase', amount: 8})
+  }
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { isVisible && <div className={classes.value}>{counter}</div> }
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseBy5Handler}>Increase By 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, toggleVisible: true };
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
         return {
             counter: state.counter + 1,
+            toggleVisible: state.toggleVisible
+        }
+    }
+
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.amount,
+            toggleVisible: state.toggleVisible
         }
     }
 
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1,
+            toggleVisible: state.toggleVisible
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            toggleVisible: !state.toggleVisible,
+            counter: state.counter,
         }
     }
 
